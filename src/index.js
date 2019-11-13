8cf42b5b62f251d5f1d3504edc94ae20aa8ff1e9
malbonesi <mikealbo@gmail.com>
2019-11-13 15:51:07 -0500

    fix formatting

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import Controller from './controllers/controller'
 const App = Mn.Application.extend({
 
   onStart() {
-		let controller = new Controller()
-		controller.start()
+    let controller = new Controller()
+    controller.start()
     Bb.history.start()
   }
 })
