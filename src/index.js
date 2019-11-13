84c9f1ca598dc80ae79fae9725ed277d22b95168
malbonesi <mikealbo@gmail.com>
2019-11-12 23:55:25 -0500

    add controller to handle root view

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 import Bb from 'backbone'
 import Mn from 'backbone.marionette'
-import _ from 'underscore'
-//import RootView from './views/root'
-import PokemonList from './views/pokemon-list'
-import PokemonCollection from './models/pokemon-collection'
+import Controller from './controllers/controller'
 
 const App = Mn.Application.extend({
-  region: '#app',
 
   onStart() {
-    //const rootView = new RootView()
-    //this.showView(rootView)
-    const pokemonListView = new PokemonList({collection: new PokemonCollection()})
-    this.showView(pokemonListView)
-
+		let controller = new Controller()
+		controller.start()
     Bb.history.start()
   }
 })
 
-const app = new App();
-app.start();
+const app = new App()
+app.start()
